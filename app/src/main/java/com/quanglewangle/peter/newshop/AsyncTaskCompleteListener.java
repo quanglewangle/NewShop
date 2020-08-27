9d060275351e287fc43eb042403e226969574e2d
quanglewangle <peter@quanglewangle.com>
2020-08-27 20:49:11 +0100

    Initial commit

---
+++ app/src/main/java/com/quanglewangle/peter/newshop/AsyncTaskCompleteListener.java
@@ -0,0 +1,8 @@
+package com.quanglewangle.peter.newshop;
+/*
+ * Created by peter on 29/07/2017.
+ */
+
+public interface AsyncTaskCompleteListener<T> {
+    public void onTaskComplete(T result);
+}
